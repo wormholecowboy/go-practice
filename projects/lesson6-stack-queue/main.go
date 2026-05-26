e96dab384e8c98bccc26b14b9d7bd413ffa86889
wormhole cowboy <wormholecowboy@gmail.com>
2026-05-25 20:08:03 -0400

    l6

---
+++ projects/lesson6-stack-queue/main.go
@@ -0,0 +1,40 @@
+package main
+
+type Stack[T any] struct {
+	nodes []T
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(s.nodes) < 1 {
+		var dummy T
+		return dummy, false
+	}
+
+	node := s.nodes[len(s.nodes)-1]
+	s.nodes = s.nodes[:len(s.nodes)-1]
+	return node, true
+}
+
+func (s *Stack[T]) Push(node T) {
+	s.nodes = append(s.nodes, node)
+}
+
+func (s *Stack[T]) Peek() (T, bool) {
+	if len(s.nodes) < 1 {
+		var dummy T
+		return dummy, false
+	}
+	return s.nodes[len(s.nodes)-1], true
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.nodes) == 0
+}
+
+func (s *Stack[T]) Size() int {
+	return len(s.nodes)
+}
+
+func main() {
+
+}
