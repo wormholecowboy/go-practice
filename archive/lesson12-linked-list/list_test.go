09265650196a420bb431dd88a4cee8f0045217c7
wormhole cowboy <wormholecowboy@gmail.com>
2026-06-14 20:07:47 -0400

    12: added tests

--- archive/lesson12-linked-list/list_test.go
+++ archive/lesson12-linked-list/list_test.go
@@ -4,23 +4,32 @@ import "testing"
 
 func TestPushHead(t *testing.T) {
 	var tests = []struct {
-		val  int
-		want int
+		val  []int
+		want []int
 	}{
-		{val: 15, want: 15},
+		{val: []int{15}, want: []int{15}},
+			{val: []int{1,2,3}, want: []int{3,2,1}},
+			{val: []int{}, want: []int{}},
 	}
 
 	for _, test := range tests {
 		var list = List[int]{}
-		list.PushHead(test.val)
 
-		got, ok := list.PeekHead()
-		if !ok {
-			t.Fatalf("PeekHead returned no value after PushHead(%d)", test.val)
+		for _, v := range test.val {
+		list.PushHead(v)
 		}
 
-		if got != test.want {
-			t.Errorf("PushHead(%d) does not equal wanted: %d", test.val, test.want)
+		got := list.ToSlice()
+		if len(got) != len(test.want) {
+			t.Errorf("PushHead(%v): got %d len for list, expected: %d", test.val, len(got), len(test.val))
+			continue
+		}
+
+		for i := range got {
+			if got[i] != test.want[i] {
+				t.Errorf("PushHead(%v): got %v, wanted %v", test.val, got, test.want)
+				break
+			}
 		}
 
 	}
